202f5b3620ad3e4f983a53504d761029968723b9
gavin <jingheyuhk@gmail.com>
2020-03-09 20:32:00 -0700

    make the fake data for the topics categrays

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,25 @@
 <?php
 
-function route_class()
-{
-    return str_replace('.', '-', Route::currentRouteName());
+if (! function_exists('route_class')) {
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+
+}
+
+if (! function_exists('time_array_format_problem_solution')) {
+    function time_array_format_problem_solution($array_include_time)
+    {
+
+        $time_key = array('email_verified_at', 'created_at', 'updated_at');
+        foreach ($array_include_time as $key => $value) {
+            foreach ($value as $k => $v) {
+                if (in_array($k, $time_key)) {
+                    $array_include_time[$key][$k] = date("Y-m-d H:i:s", strtotime($v));
+                }
+            }
+        }
+        return $array_include_time;
+    }
 }
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,18 +2,32 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+
+        $topics = factory(Topic::class)->times(100)->make()->each(function ($topic, $index)
+        use ($user_ids, $category_ids, $faker)
+        {
+            $topic->user_id = $faker->randomElement($user_ids);
+
+            $topic->category_id = $faker->randomElement($category_ids);
+
         });
 
-        Topic::insert($topics->toArray());
+        Topic::insert(time_array_format_problem_solution($topics->toArray()));
     }
 
 }
