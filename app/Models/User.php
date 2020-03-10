1acc355faa7c4561eb71eedc19876922acd7f6b2
gavin <jingheyuhk@gmail.com>
2020-03-10 02:48:52 -0700

    my topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,4 +23,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
 }
