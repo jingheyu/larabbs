5eccb248b4e3371d3d3cfddbd7cdc115e4c0f62f
gavin <jingheyuhk@gmail.com>
2020-03-10 20:51:26 -0700

    fix the git problem and topics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Observers\TopicObserver;
+use App\Models\Topic;
 class AppServiceProvider extends ServiceProvider
 {
     /**
