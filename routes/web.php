7738e18f3c3fbf6a6392ce5b4a4675bc5e1bf22d
gavin <jingheyuhk@gmail.com>
2020-03-10 23:46:49 -0700

    reply commit

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
-
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
