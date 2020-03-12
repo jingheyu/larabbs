79fa358c5784f2d6f907f7d2516f622c2961446d
gavin <jingheyuhk@gmail.com>
2020-03-11 22:22:39 -0700

    提交消息

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,16 +4,26 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
+use App\Notifications\TopicReplied;
+
+
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
+
     public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
+
+
 
     public function creating(Reply $reply)
     {
